ad922469faa7edffaa7b2d823c128d00057b3f3d
SrinathJettaboina <42053288+SrinathJettaboina@users.noreply.github.com>
2020-11-25 23:20:51 +0400

    Add files via upload

---
+++ Git Hub file for evaluation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[3]:
+
+
+A= ("Hello Capstone Project Course!")
+
+
+# In[4]:
+
+
+print(A)
+
+
+# In[ ]:
+
+
+
+
